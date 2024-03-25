8fe0166f575aef1ed8d259ffe87bf34e0686e6f7
RanjithRajaramSalem <161784794+RanjithRajaramSalem@users.noreply.github.com>
2024-03-26 04:54:30 +1100

    company_logo.py

---
+++ company_logo.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+class OrderedCounter(Counter):
+    pass
+
+if __name__ == '__main__':
+    #s = input()
+    [print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
+    
